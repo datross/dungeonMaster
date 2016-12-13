480ecdbc96710b5300d95e91dc8a21503259c214
datross <datross@openmailbox.org>
2016-12-13 17:25:18 +0100

    added meshes with simple shader

--- res/shaders/triangle.vs.glsl
+++ res/shaders/triangle.vs.glsl
@@ -1,11 +1,11 @@
 #version 330
 
-layout(location = 3) in vec2 aVertexPosition;
-layout(location = 8) in vec3 aVertexColor;
+layout(location = 0) in vec2 aVertexPosition;
+//layout(location = 1) in vec3 aVertexColor;
 
-out vec3 vColor;
+//out vec3 vColor;
 
 void main() {
-    vColor = aVertexColor;
-    gl_Position = vec4(aVertexPosition, 0, 1);
+    //vColor = aVertexColor;
+    gl_Position = vec4(0.1*aVertexPosition, 0, 1);
 }
